fdfdf749b70cc9e59e88d1b6d279633f6ab55b8c
Troyer117 <tjustesen08@gmail.com>
2025-06-14 19:34:35 -0600

    character done

---
+++ character-card/script.js
@@ -0,0 +1,43 @@
+const character = {
+  name: "Snortleblat",
+  class: "Swamp Beat Diplomat",
+  level: 5,
+  health: 100,
+  image: 'https://andejuli.github.io/img/snortleblat.png',
+  attacked() {
+    if (this.health >= 20) {
+      this.level -= 1;
+      this.health -= 20;
+    } else {
+      alert('Character Died');
+    }
+  },
+  levelUp() {
+    this.level += 1;
+    this.health += 20;
+  }
+};
+
+const nameEl = document.getElementById("name");
+const classEl = document.getElementById("class");
+const levelEl = document.getElementById("level");
+const healthEl = document.getElementById("health");
+
+function updateDisplay() {
+  nameEl.textContent = character.name;
+  classEl.textContent = character.class;
+  levelEl.textContent = character.level;
+  healthEl.textContent = character.health;
+}
+
+document.getElementById("attacked").addEventListener("click", () => {
+  character.attacked();
+  updateDisplay();
+});
+
+document.getElementById("levelup").addEventListener("click", () => {
+  character.levelUp();
+  updateDisplay();
+});
+
+updateDisplay();
